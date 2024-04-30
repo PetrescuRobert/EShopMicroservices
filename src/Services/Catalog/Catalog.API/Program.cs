5a248807cf014f589eb1c682fe5e89d51228f823
Robert <petrescu.robert69@gmail.com>
2024-04-30 15:22:14 +0300

    POST create product endpoint & register services

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the IOC container
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
+app.MapCarter();
 
 app.Run();
