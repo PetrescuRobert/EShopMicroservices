c67435fc8278d36dc118cc1544e60e4871f16941
Robert <petrescu.robert69@gmail.com>
2024-05-21 15:31:36 +0300

    Seed and Auto migrate data on application startup

--- src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -7,4 +7,12 @@ namespace Discount.gRPC.Data;
 public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
 {
     public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung S10", Description = "Samsung Discount", Amount = 100 }
+            );
+    }
 }
