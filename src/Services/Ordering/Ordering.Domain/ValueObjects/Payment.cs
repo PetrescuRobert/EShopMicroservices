bb6fc95e4e97505c6f7528794b587e601f08b639
Robert <petrescu.robert69@gmail.com>
2024-06-04 14:55:58 +0300

    Abstractions Models and ValueObjects for the Domain model

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.ValueObjects;
+public record Payment
+{
+    public string? CardName { get; } = default!;
+    public string CardNumer { get; } = default!;
+    public string Expiration { get; } = default!;
+    public string CVV { get; } = default!;
+    public int PaymentMethod { get; } = default!;
+}
