022e2a56d3cf7c256470c676d63ae049e7e8c7db
Robert <petrescu.robert69@gmail.com>
2024-07-04 13:23:35 +0300

    Enpoints for Order API

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,9 +1,31 @@
-﻿namespace Ordering.API.Endpoints;
+﻿
+using Ordering.Application.Orders.Commands.CreateOrder;
 
-public class CreateOrder
+namespace Ordering.API.Endpoints;
+public record CreateOrderRequest(OrderDto Order);
+public record CreateOrderResponse(Guid Id);
+public class CreateOrder : ICarterModule
 {
     // Accepts a CreateOrderRequest object
     // Map the request to a CreateOrderCommand
     // Use MediatR to send the command to the corresponding handler.
     // Returns a response with the created order's ID.
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+        {
+            var comand = request.Adapt<CreateOrderCommand>();
+
+            var result = await sender.Send(comand);
+
+            var response = result.Adapt<CreateOrderResponse>();
+
+            return Results.Created($"/orders/{response.Id}", response);
+        })
+            .WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
+    }
 }
